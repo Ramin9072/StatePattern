c2586fab3c13ec77c17251095d696ae388a670c0
r.navabian <r.navabian@NAVABIAN>
2023-01-31 13:53:26 +0330

    Add project files.

---
+++ StatePattern/Class/LoanRequest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.Class
+{
+    public class LoanRequest
+    {
+        public LoanRequestState State { get; set; }
+        public long Amount { get; private set; }
+        public TimeSpan LoanDuration { get; private set; }
+        public LoanRequest()
+        {
+            this.State = new DraftState();
+        }
+        public void Update(long amount, TimeSpan duration)
+        {
+            if (State.CanUpdateProgress())
+            {
+                Amount = amount;
+                LoanDuration = duration;
+            }
+            else
+            {
+                throw new Exception("!!");
+            }
+        }
+
+        public void Accept()
+        {
+            if (this.State.CanAccepted())
+            {
+                State = new AcceptedState();
+            }
+            //if (State == LoanRequestState.InProgress)
+            //{
+            //    State = LoanRequestState.Accepted;
+            //}
+            //else
+            //{
+            //    throw new Exception("!!!");
+            //}
+        }
+    }
+    //LoanRequestState() پیدا سازی جدید
+    //public enum LoanRequestState
+    //{
+    //    Draft,
+    //    InProgress,
+    //    Rejected,
+    //     
+    //}
+}
---
+++ StatePattern/Class/LoanRequestState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.Class
+{
+    public abstract class LoanRequestState
+    {
+        public virtual bool CanUpdateProgress() => false;
+        public virtual bool CanAccepted() => false;
+    }
+    public class DraftState : LoanRequestState
+    {
+        public override bool CanUpdateProgress() => true;
+
+    }
+    public class InProgressState : LoanRequestState
+    {
+        public override bool CanUpdateProgress() => true;
+    }
+    public class RejectedState : LoanRequestState
+    {
+        public virtual bool CanAccepted() => true; 
+
+    }
+    public class AcceptedState : LoanRequestState
+    {
+        public virtual bool CanAccepted() => true;
+    }
+}
